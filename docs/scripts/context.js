33d187f596dfb2501fbc89ca4acfbade645fa67f
Andrew Miner <andrewminer@mac.com>
2020-06-15 09:01:06 -0600

    Deployment of 1792cea at 2020-06-15T15:01:06.002626Z

--- docs/scripts/context.js
+++ docs/scripts/context.js
@@ -2,8 +2,8 @@
 
 window.pinerylabs = {
     application: "runbooks.app",
-    deployed_on: "2020-06-02T00:00:46.209640Z",
+    deployed_on: "2020-06-15T15:01:06.002626Z",
     environment: "staging",
-    git_hash:    "19d37ac",
-    version:     "0.4.1"
+    git_hash:    "1792cea",
+    version:     "0.5.0"
 };
